672e64e30ac473de8bb786bdbef42acaea2918d1
alexander gonzalez <alexanderegonzalez89@gmail.com>
2026-02-16 07:36:31 -0600

    test: final trigger to verify history with quality gates

--- cypress/e2e/test/prueba1.cy.ts
+++ cypress/e2e/test/prueba1.cy.ts
@@ -10,7 +10,7 @@ describe('test de inicio ', () => {
     }
     // Deje que otros errores sigan fallando la prueba por seguridad
   });
-  it('prueba1 @allure.id:1', () => {
+  it('prueba1 ✅ @allure.id:1', () => {
     allure.owner('alex')
     allure.parameter('Environment', 'Stagingtest')
     allure.parameter("time", new Date().toLocaleString("es-SV", { timeZone: "America/El_Salvador", hour12: false }), { excluded: true });
